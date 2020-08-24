14af0c2dbb5b2ff1df9d15fbe75f4f77fac8a0fa
Pob <590650@gmail.com>
2020-08-24 23:50:56 +0700

    done post-confirm auth implementation

---
+++ amplify/backend/function/amplifylambdatrigger2fe809a3PostConfirmation/src/add-to-group.js
@@ -0,0 +1,41 @@
+/* eslint-disable-line */ const aws = require('aws-sdk')
+
+exports.handler = async (event, context, callback) => {
+  const cognitoProvider = new aws.CognitoIdentityServiceProvider({ apiVersion: '2016-04-18' })
+
+  let isAdmin = false
+  const adminEmails = ['pob.ch1@yandex.com']
+
+  if (adminEmails.indexOf(event.request.userAttributes.email) !== -1) {
+    isAdmin = true
+  }
+
+  const groupParams = {
+    UserPoolId: event.userPoolId,
+  }
+
+  const userParams = {
+    UserPoolId: event.userPoolId,
+    Username: event.userName,
+  }
+
+  if (isAdmin) {
+    groupParams.GroupName = 'Admin'
+    userParams.GroupName = 'Admin'
+
+    try {
+      await cognitoProvider.getGroup(groupParams).promise()
+    } catch (e) {
+      await cognitoProvider.createGroup(groupParams).promise()
+    }
+
+    try {
+      await cognitoProvider.adminAddUserToGroup(userParams).promise()
+      callback(null, event)
+    } catch (e) {
+      callback(e)
+    }
+  } else {
+    callback(null, event)
+  }
+}
--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react'
+import './App.css'
+import { Auth } from 'aws-amplify'
+import { AmplifySignOut, withAuthenticator } from '@aws-amplify/ui-react'
 
 function App() {
+  const [user, setUser] = useState(null)
+  useEffect(() => {
+    Auth.currentAuthenticatedUser()
+      .then((user) => {
+        console.log('user:', user)
+        setUser(user)
+      })
+      .catch((e) => console.log('error:', e))
+  }, [])
+
+  let isAdmin = false
+  if (user) {
+    const {
+      signInUserSession: {
+        idToken: { payload },
+      },
+    } = user
+    console.log('payload:', payload)
+    if (payload['cognito:groups'] && payload['cognito:groups'].includes('Admin')) {
+      isAdmin = true
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1>Howdy</h1>
+        {isAdmin && <p>Welcome, Admin</p>}
       </header>
+      <AmplifySignOut />
     </div>
-  );
+  )
 }
 
-export default App;
+export default withAuthenticator(App)
